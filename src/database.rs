67f659fa7ae95bb220416966ed3f2e8c2fc07051
Hjort <shj@concordium.com>
2022-02-10 16:04:15 +0100

    Create database tables at startup

--- src/database.rs
+++ src/database.rs
@@ -5,10 +5,18 @@ const READ_RATE_STATEMENT: &str =
     "insert into read_values (value, timestamp) values (:value, :timestamp)";
 const UPDATE_RATE_STATEMENT: &str = "insert into updates (numerator, denominator, timestamp) \
                                      values (:numerator, :denominator, :timestamp)";
+const CREATE_TABLES: &str = "CREATE TABLE IF NOT EXISTS read_values (value DOUBLE NOT NULL, \
+                             timestamp DATETIME NOT NULL); CREATE TABLE IF NOT EXISTS updates \
+                             (numerator BIGINT UNSIGNED NOT NULL, denominator BIGINT UNSIGNED NOT \
+                             NULL, timestamp DATETIME NOT NULL)";
 
 pub fn establish_connection_pool(url: &str) -> mysql::Result<Pool> {
     Pool::new(Opts::from_url(url)?)
 }
+
+/// Creates the tables, we are inserting data into. (If they don't exist
+/// already)
+pub fn create_tables(conn: &mut PooledConn) -> mysql::Result<()> { conn.query_drop(CREATE_TABLES) }
 
 pub fn write_read_rate(conn: &mut PooledConn, value: f64) -> mysql::Result<()> {
     let statement = conn.prep(READ_RATE_STATEMENT)?;
